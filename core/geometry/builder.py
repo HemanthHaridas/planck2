a7e0bba16012122987575142d4c55d63f8741b2b
Hemanth Haridas <hemanthhari23@gmail.com>
2024-02-05 12:12:14 -0700

    adding files

---
+++ core/geometry/builder.py
@@ -0,0 +1,27 @@
+"""
+    Planck2 is a Python code to perform Hartree-Fock calculations on molecules.
+    Copyright (C) 2024  Hemanth Haridas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import numpy    as np
+import math     as math
+import sys      as sys
+import os       as os
+import time     as time
+
+class Molecule:
+    def __init__(self) -> None:
+        pass
