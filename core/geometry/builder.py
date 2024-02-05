900bf0de98ad5d28d48758e9ba3a4995437a59bb
Hemanth Haridas <hemanthhari23@gmail.com>
2024-02-05 14:16:40 -0700

    edited builder module

--- core/geometry/builder.py
+++ core/geometry/builder.py
@@ -16,11 +16,12 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import  numpy    as numpy
-import  math     as math
-import  sys      as sys
-import  os       as os
-import  time     as time
+import  numpy       as numpy
+import  math        as math
+import  sys         as sys
+import  os          as os
+import  time        as time
+import  tempfile    as  tmp
 
 class Molecule:
     def __init__(self) -> None:
@@ -118,12 +119,98 @@ class Molecule:
                 self.atomNames.append(_atom.split()[0])
                 self.atomCoordinates[_atom_index,:] =   [float(coord) for coord in _atom.split()[1:]]
     
-    def create_molecule_zmatrix_from_input(self, zmat: str) -> None:
-        pass
+    def create_molecule_zmatrix_from_input(self, zmat: str, charge: int, multiplicity: int) -> None:
+            self.charge             =   charge
+            self.multiplicity       =   multiplicity
+            _internal_coordinates   =   zmat.split('\n')[1:-1]
+            _natoms                 =   len(_internal_coordinates)
 
-    def create_molecule_cartesian_from_input(self, cartesian: str) -> None:
-        pass
-    
+            self.natoms             =   _natoms
+            self.atomCoordinates    =   numpy.zeros((_natoms, 3))
+            self.atomNumbers        =   numpy.zeros(_natoms)
+
+            # if only one atom, place it at origin
+            self.atomNames.append(_internal_coordinates[0].split()[0])
+            self.atomCoordinates[0,:]   =   numpy.zeros(3)
+
+            # for second atom, we need a distance, and place it
+            # along the positive x-axis
+
+            self.atomNames.append(_internal_coordinates[1].split()[0])
+            self.atomCoordinates[1][0]  =   float(_internal_coordinates[1].split()[2])
+        
+            # generate a coordinate frame with the connecting atom 
+            # at the center and the next atom along the x-axis. This
+            # places the third atom along the x-y plane.
+
+            self.atomNames.append(_internal_coordinates[2].split()[0])
+            _bond_length    =   float(_internal_coordinates[2].split()[2])
+            _bond_angle     =   float(_internal_coordinates[2].split()[4])
+            _index_atom     =   int(_internal_coordinates[2].split()[1]) - 1
+            _next_atom      =   int(_internal_coordinates[2].split()[3]) - 1
+            if _index_atom == 1:
+                _bond_angle =   180 - _bond_angle
+
+            # calculate the unit vector with the bond angle
+            self.atomCoordinates[2][0]  =   _bond_length * math.cos(_bond_angle * math.pi/180)
+            self.atomCoordinates[2][1]  =   _bond_length * math.sin(_bond_angle * math.pi/180)
+            self.atomCoordinates[2, :]  =   self.atomCoordinates[2, :] + self.atomCoordinates[_index_atom, :]
+
+            # now place the remaining atoms
+            for _atom_index in range(3, _natoms):
+                self.atomNames.append(_internal_coordinates[_atom_index].split()[0])
+                _bond_length    =   float(_internal_coordinates[_atom_index].split()[2])
+                _bond_angle     =   180 - float(_internal_coordinates[_atom_index].split()[4])
+                _bond_dihedral  =   180 - float(_internal_coordinates[_atom_index].split()[6])
+                _index_atom     =   int(_internal_coordinates[_atom_index].split()[1]) - 1
+                _second_atom    =   int(_internal_coordinates[_atom_index].split()[3]) - 1
+                _third_atom     =   int(_internal_coordinates[_atom_index].split()[5]) - 1 
+
+                _location_x     =   _bond_length * math.cos(_bond_angle * math.pi/180)
+                _location_y     =   _bond_length * math.sin(_bond_angle * math.pi/180) * math.cos(_bond_dihedral * math.pi/180)
+                _location_z     =   _bond_length * math.sin(_bond_angle * math.pi/180) * math.sin(_bond_dihedral * math.pi/180)
+                
+                _vector_ab          =   self.atomCoordinates[_third_atom, :] - self.atomCoordinates[_second_atom, :]
+                _vector_bc          =   self.atomCoordinates[_index_atom, :] - self.atomCoordinates[_second_atom, :]
+                _vector_ab          =   _vector_ab / numpy.linalg.norm(_vector_ab)
+                _vector_bc          =   _vector_bc / numpy.linalg.norm(_vector_bc)
+                _normal_abc         =   numpy.cross(_vector_ab, _vector_bc) 
+                _normal_abc         =   _normal_abc / numpy.linalg.norm(_normal_abc)
+                _normal_bcd         =   numpy.cross(_normal_abc, _vector_bc)
+                _transform_matrix   =   numpy.zeros((3, 3))
+
+                _transform_matrix[:, 0] =   _vector_bc
+                _transform_matrix[:, 1] =   _normal_bcd
+                _transform_matrix[:, 2] =   _normal_abc
+                _transformed_point      =   numpy.dot(_transform_matrix, numpy.array([_location_x, _location_y, _location_z]))
+                
+                self.atomCoordinates[_atom_index, :]    =   _transformed_point + self.atomCoordinates[_index_atom, :]
+
+
+    def create_molecule_cartesian_from_input(self, cartesian: str, charge: int, multiplicity: int) -> None:
+        self.charge             =   charge
+        self.multiplicity       =   multiplicity
+        _cartesian_coordinates  =   cartesian.split('\n')[1:-1]   
+        _natoms                 =   len(_cartesian_coordinates)
+
+        self.natoms             =   _natoms
+        self.atomCoordinates    =   numpy.zeros((_natoms, 3))
+        self.atomNumbers        =   numpy.zeros(_natoms)
+
+        for _atom_index, _atom in enumerate(_cartesian_coordinates):
+            self.atomNames.append(_atom.split()[0])
+            self.atomCoordinates[_atom_index,:] =   [float(coord) for coord in _atom.split()[1:]]
+
     def print_cartesian_coordinates(self) -> None:
         for _atomname, _atomcoords in zip(self.atomNames, self.atomCoordinates):
             print("{:<5s}{:10.3f}{:10.3f}{:10.3f}".format(_atomname, _atomcoords[0], _atomcoords[1], _atomcoords[2]))
+
+water = """
+H
+O   1   1.1
+H   2   1.1     1   109.45
+"""
+
+water_molecule  =   Molecule()
+water_molecule.create_molecule_zmatrix_from_input(zmat=water, charge=0, multiplicity=1)
+water_molecule.print_cartesian_coordinates()
